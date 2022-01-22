d89f45fe0db53434736200b93d1b1905cb3ba72e
Roshan_D <68422709+RoshanD123@users.noreply.github.com>
2022-01-22 13:01:01 +0530

    Create main.py

--- main.py
+++ main.py
@@ -59,24 +59,34 @@ def symptoms():
   fever = str(input("Do you have a fever?"))
   night_sweats = str(input("Do you sweat in the night?"))
   chills = str(input("Have you been getting chills recently?"))
-  headache = str(input("Hav you been getting headaches recently?"))
+  headache = str(input("Have you been getting headaches recently?"))
   loss_of_smell = str(input("Have you lost your sense of smell?"))
   fatigue = str(input("Have you been feeling tired and getting a lot of fatigue?"))
   Time=int(input("How long did it take for symptoms to show"))
   listof=["Cancer","Chronic kidney disease","Chronic Lung disease","Dementia","Neurological Conditions","Diabetes","Down Syndrome","Heart conditions","HIV","Weakened Immune system","Mental Health conditions","Overweight and obesity","Sickle cell disease","smoking, current of former","substance use disorders","Tuberculosis"]
   print(listof)
   dis=str(input("Do you find yourself in any of these conditions and diseases?"))
-  if dis=="yes" and cough=="yes" and runny_nose=="yes" and sneezing=="yes" and shortness_of_breath=="yes" and sore_throat=="yes"and fever=="yes" and night_sweats=="yes" and chills=="yes" and headache=="yes" and loss_of_smell=="yes" and fatigue=="yes":
-    print("please take appropriate precation moving forward.")
+  if dis=="yes" and cough=="yes" or runny_nose=="yes" or sneezing=="yes" or shortness_of_breath=="yes" or sore_throat=="yes"or fever=="yes" or night_sweats=="yes" or chills=="yes" or headache=="yes" or loss_of_smell=="yes" or fatigue=="yes":
+    print("You are in an extreme chronic condition and so go to a doctor as soon as possible.")
   
   elif dis=="yes" and cough=="no" and runny_nose=="no" and sneezing=="no" and shortness_of_breath=="no" and sore_throat=="no" and fever=="no" and night_sweats=="no" and chills=="no" and headache=="no" and loss_of_smell=="no" and fatigue=="no":
-    print("be careful as you are in dagerous condition.")
+    print("Be careful as you are in dangerous condition, and maintain COVID protocols all the time to ensure your safety.")
   
-  elif dis=="no" and cough=="yes" and runny_nose=="yes" and sneezing=="yes" and shortness_of_breath=="yes" and sore_throat=="yes"and fever=="yes" and night_sweats=="yes" and chills=="yes" and headache=="yes" and loss_of_smell=="yes" and fatigue=="yes":
-    print("")
+  elif dis=="no" and cough=="yes" or runny_nose=="yes" or sneezing=="yes" or shortness_of_breath=="yes" or sore_throat=="yes"or fever=="yes" or night_sweats=="yes" or chills=="yes" or headache=="yes" or loss_of_smell=="yes" or fatigue=="yes":
+    print("Please continue")
+    if night_sweats=="yes":
+      print("50% chance  for Omnicron Variant of COVID"
+    elif night_sweats=="no":
+            print("50% chance for not having Omnicron Varinat COVID")
+    elif sneezing=="no":
+            print("100% chance for not having Delta Variant COVID or Flu")
+    elif sneezing=="yes":
+            print("100% chance to have Common Cold or Omnicron variant COVID")
+    elif shortness_of_breath=="yes":
+            print("75% chance for having Delta Variant of COVID")
   
   elif dis=="no" and cough=="no" and runny_nose=="no" and sneezing=="no" and shortness_of_breath=="no" and sore_throat=="no" and fever=="no" and night_sweats=="no" and chills=="no" and headache=="no" and loss_of_smell=="no" and fatigue=="no":
-    print("ef")
+    print("You are in no danger, but be careful and follow all COVID protocols")
   
 
   
@@ -92,13 +102,11 @@ if Age>=60:
     print("Good Job, but take a booster dose to increase your safety")  
     symptoms() 
   else:
-    print("Great Job, now follow all COVID precautions")
+    print("Great Job, now follow all COVID precautions like putting on mask and so on.")
     symptoms()
 else:
   print("Please take the required amount of doses for your age, and follow corona protocols at all times.")
   symptoms()
-
-
 
 
 
